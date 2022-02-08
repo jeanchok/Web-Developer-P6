1abdcdeadc4730428cf8d0b8779b3173f87922f3
jeanchok <48122647+jeanchok@users.noreply.github.com>
2022-02-08 18:19:23 +0100

    ok

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -7,8 +7,8 @@ const multer = require('../middleware/multer-config');
 const saucesCtrl = require('../controllers/sauces');
 
 router.get('/', auth, saucesCtrl.getAllSauces);
-router.post('/', auth, multer, saucesCtrl.createSauces);
 router.get('/:id', auth, saucesCtrl.getOneSauces);
+router.post('/', auth, multer, saucesCtrl.createSauces);
 router.put('/:id', auth, multer, saucesCtrl.modifySauces);
 router.delete('/:id', auth, saucesCtrl.deleteSauces);
 router.post('/:id/like', auth, saucesCtrl.likeDislikeSauce);
